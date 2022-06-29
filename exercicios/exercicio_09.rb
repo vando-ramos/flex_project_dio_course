b5caae238a10c64c21ec6b87b99b8cbbd0587a08
Vando <vandorramos@gmail.com>
2022-06-29 11:32:15 -0300

    exercicios deletados

--- exercicios/exercicio_09.rb
+++
@@ -1,9 +0,0 @@
-=begin
-Faça um algoritmo que receba um valor que foi depositado e exiba o valor com rendimento após um mês. 
-Considere fixo o juro da poupança em 0,70% a. m.
-=end
-
-puts 'Deposite na sua poupança:'
-valor = gets.chomp.to_f
-rend = valor + (valor * 0.007)
-puts "Rendimento após um mês = #{rend}"
--- exercicios/exercicio_13.rb
+++
@@ -1,9 +0,0 @@
-# Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 10.
-
-
-puts 'Didgite um número:'
-number = gets.chomp.to_i
-
-if number > 10
-    puts 'Número maior que 10'
-end
